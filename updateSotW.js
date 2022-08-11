ed35c93b299a42fb3e7db197ca457488b9fb1798
jaymesgarbe <jrgarbe7@gmail.com>
2022-08-10 22:51:14 -0700

    added non-working button and such

---
+++ updateSotW.js
@@ -0,0 +1,33 @@
+// automate Song of the Week updating. . . 
+function updateSotW() {
+    console.log('clicked!')
+
+    // adding current song to archive
+    const titleEl = document.getElementsById('title')
+    const archive = document.getElementsById('archive')
+    archive.insertAdjacentHTML('beforeend',`<p><b><center>${titleEl.innerText}</center></b></p>`)
+
+    // changing title to new SotW
+    var newTitle = prompt(`New SotW -- Old Title: ${titleEl}`)
+    titleEl.innerText(newTitle)
+
+    // changing rating
+    var rating = document.getElementById('rating')
+    var newRating = prompt('What do you rate this Song')
+    rating.innerText(`Overall Rating: ${newRating}/10`)
+
+    // changing fav part
+    var favPart = document.getElementsById('favPart')
+    var newFavPart = prompt('What is your fav part of this song')
+    favPart.innerText(newFavPart)
+
+    // changing overall review
+    var overall = document.getElementsById('overall')
+    var newOverall = prompt('What is your overall review of this song')
+    overall.innerText(newOverall)
+
+    // changing img src
+    var newImgSrc = prompt('Import new thumbnail... input file name')
+    document.getElementById('imgSrc').src = `images/${newImgSrc}`
+}
+
