31589b191f75700b3f2b8e366b762fce3ea51577
jaymesgarbe <jrgarbe7@gmail.com>
2022-08-15 19:28:38 -0700

    popup working and writing to sheets

--- updateSotW.js
+++ updateSotW.js
@@ -1,9 +1,25 @@
-// automate Song of the Week updating. . . 
+// automate Song of the Week updating. . .
+
+window.onload = function () {
+  const form = document.getElementById("newsletterForm");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Success!");
+      document.getElementById("buttonPopup").hidden = true;
+    });
+  });
+};
 
 function test() {
-    alert('just testing :)')
+  alert("just testing :)");
 }
 
 function addToNewsletter() {
-    document.getElementById('buttonPopup').hidden = false
+  document.getElementById("buttonPopup").hidden = false;
 }
